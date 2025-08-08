292cce41486314a90d18e005b97e286e2009b403
MCKIT77 <jakemckit@gmail.com>
2025-08-08 09:23:18 -0400

    fixed footer positioning

--- cooper-customs-llc/src/components/Footer.jsx
+++ cooper-customs-llc/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="mt-auto text-dark p-4">
+        <footer className="profile-footer mt-auto text-dark p-4">
             <div className="container text-center mb-5">
 
 
